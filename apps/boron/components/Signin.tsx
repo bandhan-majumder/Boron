367c823ca02a16892b6e9a7f0b7177ac3517bcb4
Bandhan Majumder <bandhanmajumder16@gmail.com>
2025-10-31 00:43:48 +0530

    google svg

--- apps/boron/components/Signin.tsx
+++ apps/boron/components/Signin.tsx
@@ -57,16 +57,15 @@ export default function Signin({
               disabled={clicked}
               variant="outline"
               type="button"
-              className="w-full text-gray-900 font-medium flex items-center justify-center gap-3 py-6 text-md"
+              className="w-full text-white flex items-center justify-center gap-3 py-6 bg-[#141413] border border-gray-700 hover:bg-[#000000] hover:text-white text-lg"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 30 30"
-                fill="currentColor"
-                className="size-6 md:size-8 text-blue-400"
-              >
-                <path d="M 15.003906 3 C 8.3749062 3 3 8.373 3 15 C 3 21.627 8.3749062 27 15.003906 27 C 25.013906 27 27.269078 17.707 26.330078 13 L 25 13 L 22.732422 13 L 15 13 L 15 17 L 22.738281 17 C 21.848702 20.448251 18.725955 23 15 23 C 10.582 23 7 19.418 7 15 C 7 10.582 10.582 7 15 7 C 17.009 7 18.839141 7.74575 20.244141 8.96875 L 23.085938 6.1289062 C 20.951937 4.1849063 18.116906 3 15.003906 3 z"></path>
-              </svg>
+              <Image
+                crossOrigin="anonymous"
+                src={"/google.svg"}
+                width={30}
+                height={30}
+                alt="logo"
+              />
               {!clicked ? "Continue with Google" : "Signing in.."}
             </Button>
           </div>
